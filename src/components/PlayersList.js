a55173928e4754a24e9df29c24038a51d696b81c
JeremiahFailla <jpf405@gmail.com>
2021-12-08 15:18:26 -0500

    Added Pagination Buttons with most of logic

--- src/components/PlayersList.js
+++ src/components/PlayersList.js
@@ -1,4 +1,4 @@
-import React, { Fragment } from "react";
+import React, { Fragment, useState, useEffect } from "react";
 import DisplayStats from "./displayStats/DisplayStats";
 import classes from "./styles/PlayersList.module.css";
 import { useSelector, useDispatch } from "react-redux";
@@ -7,6 +7,10 @@ const PlayersList = (props) => {
   const modalOpen = useSelector((state) => state.modalState);
   const selectedPlayer = useSelector((state) => state.selectedPlayer);
   const dispatch = useDispatch();
+  const [searchedPlayersObj, setSearchedPlayersObj] = useState({
+    players: props.players,
+    page: 0,
+  });
 
   const openModal = () => {
     dispatch({ type: "openModal" });
@@ -21,42 +25,89 @@ const PlayersList = (props) => {
     clickedPlayer(player);
   };
 
+  const onPrevBtnHandler = () => {
+    if (searchedPlayersObj.page === 0) return;
+    setSearchedPlayersObj({
+      ...searchedPlayersObj,
+      page: searchedPlayersObj.page - 1,
+    });
+  };
+
+  const onNextBtnHandler = () => {
+    const numPages = Math.floor(searchedPlayersObj.players.length / 9);
+    console.log(numPages);
+    if (searchedPlayersObj.page === numPages) return;
+    setSearchedPlayersObj({
+      ...searchedPlayersObj,
+      page: searchedPlayersObj.page + 1,
+    });
+  };
+
   let content;
 
   if (props.type === "click") {
     content = (
       <div className={classes.grid}>
-        {props.players.map((player) => (
-          <div
-            className={classes.playerCard}
-            key={player.id}
-            onClick={onPlayerClick.bind(this, player)}
-          >
-            <div className={`flex-c ${player.rating} ${classes.rating}`}>
-              {player.overall}
+        {searchedPlayersObj.players
+          .slice(
+            0 + searchedPlayersObj.page * 9,
+            9 + searchedPlayersObj.page * 9
+          )
+          .map((player) => (
+            <div
+              className={classes.playerCard}
+              key={player.id}
+              onClick={onPlayerClick.bind(this, player)}
+            >
+              <div className={`flex-c ${player.rating} ${classes.rating}`}>
+                {player.overall}
+              </div>
+              <div>{player.name}</div>
             </div>
-            <div>{player.name}</div>
-          </div>
-        ))}
+          ))}
+        <div className={classes.paginationContainer}>
+          <button type="button">next</button>
+        </div>
         {modalOpen && <DisplayStats player={selectedPlayer} />}
       </div>
     );
   } else if (props.type === "double") {
     content = (
       <div className={classes.grid}>
-        {props.players.map((player) => (
-          <div
-            className={classes.playerCard}
-            key={player.id}
-            onDoubleClick={onPlayerClick.bind(this, player)}
-            onClick={clickedPlayer.bind(this, player)}
-          >
-            <div className={`flex-c ${player.rating} ${classes.rating}`}>
-              {player.overall}
+        {searchedPlayersObj.players
+          .slice(
+            0 + searchedPlayersObj.page * 9,
+            9 + searchedPlayersObj.page * 9
+          )
+          .map((player) => (
+            <div
+              className={classes.playerCard}
+              key={player.id}
+              onDoubleClick={onPlayerClick.bind(this, player)}
+              onClick={clickedPlayer.bind(this, player)}
+            >
+              <div className={`flex-c ${player.rating} ${classes.rating}`}>
+                {player.overall}
+              </div>
+              <div>{player.name}</div>
             </div>
-            <div>{player.name}</div>
-          </div>
-        ))}
+          ))}
+        <div className={classes.paginationContainer}>
+          <button
+            type="button"
+            className={classes.paginationBtn}
+            onClick={onPrevBtnHandler}
+          >
+            Prev
+          </button>
+          <button
+            type="button"
+            className={classes.paginationBtn}
+            onClick={onNextBtnHandler}
+          >
+            Next
+          </button>
+        </div>
         {modalOpen && <DisplayStats player={selectedPlayer} />}
       </div>
     );
