6bb7f34df61f07eacdf5c9376c6fb4a64f25a58e
JeremiahFailla <jpf405@gmail.com>
2022-01-01 16:28:58 -0500

    fixed overflow issue when modal is open

--- src/components/modals/PlayerStatsModal.js
+++ src/components/modals/PlayerStatsModal.js
@@ -1,4 +1,4 @@
-import React, { Fragment } from "react";
+import React, { Fragment, useEffect } from "react";
 import classes from "./PlayerStatsModal.module.css";
 import ReactDOM from "react-dom";
 import { useDispatch } from "react-redux";
@@ -13,6 +13,12 @@ const Backdrop = () => {
 };
 
 const ModalOverlay = (props) => {
+  useEffect(() => {
+    document.querySelector("body").classList.add(`${classes.noScroll}`);
+    return () => {
+      document.querySelector("body").classList.remove(`${classes.noScroll}`);
+    };
+  }, []);
   return (
     <div className={classes.modal}>
       <div className={classes.content}>{props.children}</div>
@@ -23,9 +29,9 @@ const ModalOverlay = (props) => {
 const portalElement = document.getElementById("overlays");
 
 const PlayerStatsModal = (props) => {
+  // {ReactDOM.createPortal(<Backdrop />, portalElement)}
   return (
     <Fragment>
-      {ReactDOM.createPortal(<Backdrop />, portalElement)}
       {ReactDOM.createPortal(
         <ModalOverlay>{props.children}</ModalOverlay>,
         portalElement
